6e385ee29cb4aca9cc10d8c81a63c323b00cb5de
srinjoy sil <167159363+silsrinjoy23@users.noreply.github.com>
2025-08-03 23:11:59 +0530

    Update firbase.js

--- twiller/src/context/firbase.js
+++ twiller/src/context/firbase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6cfwfmIOlEcA4BWv-Ln_SKbp6HEtS_sU",
-  authDomain: "twiller-457a6.firebaseapp.com",
-  projectId: "twiller-457a6",
-  storageBucket: "twiller-457a6.firebasestorage.app",
-  messagingSenderId: "341658706580",
-  appId: "1:341658706580:web:f4ba7ea2de658c626b8a81",
-  measurementId: "G-261KS58K78"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
